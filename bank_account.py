99ea90558661129cec8141c350602239b23db492
Darragh <darraghdrennan5gmail.com>
2026-06-19 10:28:03 +0100

    Add deposit and init functions

--- bank_account.py
+++ bank_account.py
@@ -2,4 +2,12 @@ class BankAccount:
     """
     Parent class representing typical bank account.
     """
-    
+    def __init__(self, account_number, account_balance):
+        self.__account_number = account_number
+        self.__account_balance = account_balance
+
+    def deposit(self, amount):
+        """
+        Adds money to the account.
+        """
+        self.__account_balance += amount
