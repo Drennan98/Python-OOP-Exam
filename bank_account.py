0d74d5debabc603725b595239d38db92a7b06c1e
Darragh <darraghdrennan5gmail.com>
2026-06-19 10:29:48 +0100

    Add withdraw function

--- bank_account.py
+++ bank_account.py
@@ -11,3 +11,12 @@ class BankAccount:
         Adds money to the account.
         """
         self.__account_balance += amount
+
+    def withdraw(self, amount):
+        """
+        Removes money from the account if that balance is available.
+        """
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+        else:
+            print("Insufficient funds.")
